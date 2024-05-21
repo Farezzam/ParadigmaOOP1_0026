81cbbc456f82c87d94ffa98b3df0ac5f97b89bc1
ASUS <faris13abiyu@gmail.com>
2024-05-21 21:25:53 +0700

    class pasien : public

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -2,3 +2,17 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien { // asosiasi
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasen \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
+};
